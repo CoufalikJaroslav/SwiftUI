bd080734d588e2d95c4902f0d7828e8442e4ab0b
CoufalikJaroslav <jaroslav.coufalik@gmail.com>
2022-07-06 16:52:26 +0200

    Update AnimationCompletionObserverModifier.swift

--- AnimationCompletionObserverModifier.swift
+++ AnimationCompletionObserverModifier.swift
@@ -1,5 +1,48 @@
 import SwiftUI
 
+// public struct CustomView: View, Animatable {
+//     public init() {}
+    
+//     @State private var offsetVelocity: CGFloat = 100
+//     @State private var offset: CGFloat = 0
+//     @State private var rotationDegrees: CGFloat = 0
+//     @State private var scale: CGFloat = 1 {
+//         didSet {
+//             print(scale)
+//         }
+//     }
+    
+//     public var body: some View {
+//         HStack {
+//             Spacer()
+//             Text("Text")
+//                 .lineLimit(1)
+//                 .padding(.all, 10)
+//                 .frame(width: 200, height: 100)
+//                 .background(Color.yellow)
+//                 .border(.blue)
+//                 .rotationEffect(.degrees(rotationDegrees))
+//                 .offset(x: offset, y: 10)
+//                 .scaleEffect(scale)
+//             Spacer()
+//         }
+//         .frame(width: 600, height: 300)
+//         .border(.red)
+//         .clipped()
+//         .onTapGesture {
+//             withAnimation(.linear(duration: 0.5)) {
+//                 scale += 1
+//                 rotationDegrees += 90
+//                 offsetVelocity *= -1
+//                 offset += offsetVelocity
+//             }
+//         }
+//         .onAnimationCompleted(for: scale, completion: {
+//             print("finished")
+//         })
+//     }
+// }
+
 public extension View {
     func onAnimationCompleted<Value: VectorArithmetic>(for value: Value, completion: @escaping () -> Void) -> ModifiedContent<Self, AnimationCompletionObserverModifier<Value>> {
         return modifier(AnimationCompletionObserverModifier(observedValue: value, completion: completion))
